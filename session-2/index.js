a8d0fb15ff0f938e5a1449f54ada39e00ab4d531
Patience Hodzi <patiencehodzi@gmail.com>
2022-10-05 21:12:48 +0100

    console-log

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log("Hello World")
+console.log(document.querySelector('.about').innerHTML)
+console.log(document.querySelectorAll('.about'))
