927b08da98a2f55892208882768471054d66bb13
Patience Hodzi <patiencehodzi@gmail.com>
2022-10-13 11:32:17 +0100

    maths-operators

--- session-4/index.js
+++ session-4/index.js
@@ -1,8 +1,41 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+const personA = {
+    name: "Micah",
+    age: 4,
+    location: "Sutton",
+    likes: "trains",
+};
+
+const personB = {
+    name: "Naishe",
+    age:2,
+    location: "wolves",
+    likes: "peppa big"
+};
+
+const personC = {
+    name: "Aaron",
+    age: 1,
+    location: "Sutton",
+    likes: "blanky",
+};
+
+const personD = {
+    name: "adie",
+    age:0,
+    location: "wolves",
+    likes: "milk",
+}
+
+function biograghy(person) {
+return `Hi, my name is ${person.name},
+      I am ${person.age},
+      I live in ${person.location}, 
+      I like ${person.likes}`
+}
 
 
-
-
+console.log(biograghy(personA));
 
 
 
@@ -19,28 +52,37 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 6;
+ const y = 4;
 
-// const addition = x + y;
+ const addition = x + y + x;
 
-// console.log("Addition: x + y " + addition);
+ console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+ const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+ console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+ const multiplication = x * x;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+ const division = x * y;
 
-// console.log("Division: x + y " + division);
-
-
+ console.log("multiplication: x * x " + multiplication);
 
 
+ const multiplication2 = x * y * x;
+console.log("here I multiply" + " " + multiplication2);
+
+const subtraction2 = x - y + x;
+console.log("Take it away" + " " + subtraction2);
+
+const addition2 = x + y + x;
+console.log(" Add it up" + " " + addition2);
+
+const division2 = x / y * x;
+console.log("divide it" + " " + division2);
 
 
 
