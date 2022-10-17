5275e4da5450fd686c7625044d35757b3ceda9a3
Patience Hodzi <patiencehodzi@gmail.com>
2022-10-17 20:05:32 +0100

    get-grade-functin

---
+++ session-5/index.js
@@ -0,0 +1,21 @@
+function getGrade (total) {
+    if (total > 90) {
+        console.log(`A+`)
+    } else if (total > 80 && total <= 90) {
+        console.log(`A`)
+    } else if (total > 70 && total <= 80) {
+        console.log(`B`)
+    } else if (total > 60 && total <= 70) {
+        console.log(`C`)
+    } else if (total > 50 && total <= 60) {
+        console.log(`D`)
+    } else if (total > 40 && total <= 50) {
+        console.log(`E`)
+    } else if (total > 30 && total <=40) {
+        console.log(`F`)
+    } else (total > 30); {
+        console.log(`F`)
+    }
+}
+
+getGrade(55)
