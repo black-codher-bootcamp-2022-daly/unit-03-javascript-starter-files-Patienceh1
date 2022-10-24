a39d9cdb4594494fb563c3aa431848d1bddb2a85
Patience Hodzi <patiencehodzi@gmail.com>
2022-10-24 21:09:49 +0100

    Arrays

--- session-7/index.js
+++ session-7/index.js
@@ -1,12 +1,9 @@
 // ****************************************************TASK 1****************************************************
 
+const animals = ['cat', 'dog', 'pig']
 
-
-
-
-
-
-
+animals.pop()
+console.log(animals)
 
 
 
@@ -15,7 +12,30 @@
 
 
 // ****************************************************TASK 2****************************************************
+const names = [
+  'Olivia',
+  'Noah',
+  'Oliver',
+  'lily',
+  'alan',
+  'maisy',
+  'hannah',
+  'Mike',
+  'reece',
+  'emma'
+]
 
+names.reverse()
+console.log(`before`,names)
+
+names.sort()
+console.log(`after`, names)
+
+
+const numbers = [ -5, 325, 67, 100000, 150]
+
+numbers.sort()
+console.log(numbers)
 
 
 
@@ -28,15 +48,21 @@
 
 // ****************************************************TASK 3****************************************************
 
-// Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
+ //Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
 const people = [
   { name: `t'challa`, age: 37 },
   { name: 'shuri', age: 16 },
   { name: 'killmonger', age: 32 },
+
+  
 ];
 
 // This is a helper function that should be used in the final task of the session.
-// You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
-function compare(a, b) {
-  return a.age - b.age;
-}
+// You should pass this function to the sort method to sort the ages of each individiual in the people array of objectsfunction compare(a, b) {
+function compare(a,b) {
+return a.age - b.age;}
+
+
+
+const sortedPeople = people.sort(compare).map(item => item.name)
+console.log(sortedPeople);
