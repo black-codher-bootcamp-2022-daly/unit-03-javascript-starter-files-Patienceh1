e2704dfa873f65cccdf4d7e4e8b208fdcb5d0b99
Patience Hodzi <patiencehodzi@gmail.com>
2022-10-31 14:29:35 +0000

    session-8

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,35 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+
+const button = document.createElement('button')
+
+const buttonText = document.createTextNode('list of fruits')
+
+button.appendChild(buttonText)
+
+button.onclick = () => createListOfFruits(fruits)
+
+document.body.appendChild(button)
+
+
+const helloButton = document.querySelector('#remove-handler-button')
+
+helloButton.addEventListener('click', removeHandler)
+
+function removeHandler() {
+    const box = document.querySelector("#remove-handler")
+    box.removeEventListener('mousemove', randomNumber)
+}
+
+
+document
+  .querySelector("#remove-handler")
+  .addEventListener("mousemove", randomNumber);
+
+function randomNumber() {
+  document.querySelector("#random-number").innerHTML = Math.random();
+}
+
+
